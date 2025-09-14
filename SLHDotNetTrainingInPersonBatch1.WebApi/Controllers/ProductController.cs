ffb19529c3d8b07d091096f0d08236b77dddcd36
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2025-09-14 12:24:08 +0630

    Add project files.

---
+++ SLHDotNetTrainingInPersonBatch1.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,59 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace SLHDotNetTrainingInPersonBatch1.WebApi.Controllers
+{
+    // https://localhost:3000/api/Product
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly SqlConnectionStringBuilder sqlConnectionStringBuilder = new()
+        {
+            DataSource = ".",
+            InitialCatalog = "InPersonBatch1MiniPOS",
+            UserID = "sa",
+            Password = "sasa@123",
+            TrustServerCertificate = true
+        };
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            using IDbConnection db = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            db.Open();
+
+            var lst = db.Query<ProductModel>("select * from Tbl_Product").ToList();
+
+            var response = new { Message = "PLM Success - GetProducts", Data = lst };
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct()
+        {
+            return Ok("PLM Success - CreateProduct");
+        }
+
+        [HttpPut]
+        public IActionResult UpsertProduct() // Update + Insert
+        {
+            return Ok("PLM Success - UpsertProduct");
+        }
+
+        [HttpPatch]
+        public IActionResult UpdateProduct()
+        {
+            return Ok("PLM Success - UpdateProduct");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteProduct()
+        {
+            return Ok("PLM Success - DeleteProduct");
+        }
+    }
+}
